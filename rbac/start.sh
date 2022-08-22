c8af39ae7e2955ac70f28cebfa910b9a492f8616
Roberto Arcomano <bertolinux@gmail.com>
2022-08-22 23:10:04 +0200

    rbac ok

--- rbac/start.sh
+++ rbac/start.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # 0. Constants
-USER=user
+USER=user1
 NUM_BITS=2048
-KEY=user.key
-CSR=user.csr
+KEY=$USER.key
+CSR=$USER.csr
 SUBJ="/CN=$USER/O=Group"
 CAKEY=ca.key
 CA=ca.crt
-CERT=user.crt
+CERT=$USER.crt
 DAYS=1000
-ROLE=role
+ROLE=role1
+ROLEBINDING=rolebinding1
 CLUSTER=kubernetes
+CONTEXT=$USER"-"$CLUSTER
 
 # 1. Key and Certificate creation
 # 1.1. Key creation
@@ -24,13 +26,19 @@ openssl req -new -key $KEY -out $CSR -subj "$SUBJ"
 openssl x509 -req -in $CSR -CAkey "$CAKEY" -CA "$CA" -CAcreateserial -days "$DAYS" -out $CERT
 
 # 1.3. Role creation
-kubectl create role role --verb get,list --resource pod,deploy
+kubectl delete role $ROLE
+kubectl create role $ROLE --verb get,list --resource pod,deploy
 
 # 1.4. Rolebinding creation
-kubectl create rolebinding rolebinding --user $USER --role $ROLE
+kubectl delete rolebinding $ROLEBINDING
+kubectl create rolebinding $ROLEBINDING --user $USER --role $ROLE
 
 # 1.5. User creation
 kubectl config set-credentials $USER --client-key $KEY --client-certificate $CERT
 
 # 1.6. Context creation
-kubectl config set-context user-kubernetes --cluster $CLUSTER --user $USER
+kubectl config set-context $CONTEXT --cluster $CLUSTER --user $USER
+
+# 1.7. Documentation
+echo ""
+echo "You can now use: kubectl config use-context $CONTEXT"
