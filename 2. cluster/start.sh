eca6edcd1d13369c7f3870ae1b485a985c2d47b8
Roberto Arcomano <bertolinux@gmail.com>
2022-08-23 15:41:55 +0200

    cluster ok with log files

--- 2. cluster/start.sh
+++ 2. cluster/start.sh
@@ -4,15 +4,18 @@
 POD_NETWORK=10.244.0.0/16
 API_SERVER_IP=192.168.10.156
 API_SERVER_PORT=6443
-TOKEN="n8yyyq.j9cr4a4zacsj1r0w"
-TOKEN_HASH="sha256:d8cb883f468ec3b81fb04386f41fd1e48368fe36eca7f94c5bb2f7932d648a9e"
+TOKEN="m1wz9i.5jaoea1keiy9ny4o"
+TOKEN_HASH="sha256:1976e913c32bef595cc52020a16fd9887c8e0009d480169690d6964eaa8a90a2"
 
-# 1. Initialize Kubernetes cluster
+# 1. Initialize Kubernetes cluster on master node
 kubeadm init --pod-network-cidr "$POD_NETWORK" --apiserver-advertise-address "$API_SERVER_IP"
 
-# 2. Install pod network addon
+# 2. Copy configuration file on master node
+mkdir ~/.kube
+cp /etc/kubernetes/admin.conf ~/.kube/config
+
+# 3. Install pod network addon on master node
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
-# 3. Join the cluster
-kubeadm join "$API_SERVER_IP":"$API_SERVER_PORT" --token "$TOKEN" --discovery-token-ca-cert-hash "$TOKEN_HASH"
-
+# 4. Join the cluster on each worker node
+echo kubeadm join "$API_SERVER_IP":"$API_SERVER_PORT" --token "$TOKEN" --discovery-token-ca-cert-hash "$TOKEN_HASH"
