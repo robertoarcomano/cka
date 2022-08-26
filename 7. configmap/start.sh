cd5ef0044efadddbe268529cdc912a3cb70a1783
Roberto Arcomano <bertolinux@gmail.com>
2022-08-26 18:29:24 +0200

    configmap WIP1.

--- 7. configmap/start.sh
+++ 7. configmap/start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # 0. Constants
+CONFIGMAP1=cm1
+CONFIGMAP2=cm2
+CONFIGMAP3=cm3
 
 ## functions
 #function show_replicas {
@@ -8,9 +11,9 @@
 #  echo
 #}
 
-# 1. Create deploy
-# 1.1. Delete previous deploy
-kubectl delete deploy $DEPLOY
-# 1.2. Create deploy with $NUM_REPLICAS1
-kubectl create deploy $DEPLOY --image $IMAGE --replicas $NUM_REPLICAS1
-show_replicas
+# 1. Create configmap
+# 1.1. Delete previous configmap
+kubectl delete cm $CONFIGMAP1
+# 1.2. Create $CONFIGMAP1
+kubectl create cm $CONFIGMAP1 --from-literal "user=roberto" --from-literal "city=Turin"
+
