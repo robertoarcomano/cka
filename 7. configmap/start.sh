1d0b9a4768864ecd34dfb30b3c0e756ef33906a3
Roberto Arcomano <bertolinux@gmail.com>
2022-08-29 11:02:40 +0200

    configmap ok.

--- 7. configmap/start.sh
+++ 7. configmap/start.sh
@@ -9,32 +9,41 @@ FILE=file.json
 POD_FILE=nginx.yaml
 POD=nginx
 
-## functions
-#function show_replicas {
-#  kubectl rollout status deploy $DEPLOY
-#  echo
-#}
-
 # 1. Create configmaps
 # 1.0. Delete previous configmap
 kubectl delete cm $CONFIGMAP1 $CONFIGMAP2 $CONFIGMAP3
+
 # 1.1. Create $CONFIGMAP1
 kubectl create cm $CONFIGMAP1 --from-literal "user=roberto" --from-literal "city=Turin"
+
 # 1.2. Create $CONFIGMAP2
 kubectl create cm $CONFIGMAP2 --from-env-file $ENV_FILE
+
 # 1.3. Create $CONFIGMAP3
 kubectl create cm $CONFIGMAP3 --from-file $FILE
+
 # 1.4. Check
 kubectl get cm cm1 cm2 cm3 -o yaml
 
 # 2. Use configmaps
 # 2.0. Delete the pod
 kubectl delete pod $POD
+
 # 2.1. Create a pod (dry run) and save it to $POD_FILE
 kubectl run $POD --image nginx --dry-run=client -o yaml > $POD_FILE
+
 # 2.2. Add to the pod
 cat $POD_FILE | yq '
-.spec.volumes[0]={"name": "vol-cm1", "configMap": { "name": "cm1"}},
+.spec.volumes = [
+  {"name": "vol-cm1", "configMap": { "name": "cm1"}},
+  {"name": "vol-cm2", "configMap": { "name": "cm2"}},
+  {"name": "vol-cm3", "configMap": { "name": "cm3"}}
+],
+.spec.containers[0].volumeMounts = [
+  {"name": "vol-cm1", "mountPath": "/tmp/cm1"},
+  {"name": "vol-cm2", "mountPath": "/tmp/cm2"},
+  {"name": "vol-cm3", "mountPath": "/tmp/cm3"}
+],
 .spec.containers[0].env = [
   {
     "name": "user",
@@ -51,6 +60,11 @@ cat $POD_FILE | yq '
     "configMapRef": {
       "name": "cm2"
     }
+  },
+  {
+    "configMapRef": {
+      "name": "cm3"
+    }
   }
 ]
 '|kubectl apply -f -
@@ -58,5 +72,10 @@ cat $POD_FILE | yq '
 # 2.3. Sleep until pod is ready
 echo "Waiting for the pod to become Running..."
 until kubectl get pods nginx|grep Running; do sleep 1; done
-# 2.4. Check for cm1
-kubectl exec -it nginx -- env | grep -e user -e number -e country
+
+# 2.4. Check for all cm from env
+kubectl exec -it nginx -- env | grep -e user -e number -e country -e age
+
+# 2.5. Check for all cm from mounted volumes
+kubectl exec -it nginx -- bash -c "find /tmp/cm*"
+
