75a7ea6cc9a86fd0d001f567bef1947c97b890e9
Roberto Arcomano <bertolinux@gmail.com>
2022-08-30 11:28:19 +0200

    static storage: ok.

--- 11. storage/start.sh
+++ 11. storage/start.sh
@@ -1,44 +1,34 @@
 #!/bin/bash
 
 # 0. Constants
-DEPLOY1=nginx
-DEPLOY2=httpd
-IMAGE1=$DEPLOY1
-IMAGE2=$DEPLOY2
-SVC1=svc-$DEPLOY1
-SVC2=svc-$DEPLOY2
-PORT=80
-NUM_REPLICAS=2
-INGRESS=ingress1
-INGRESS_CLASS=nginx
+PV1_FILE=pv1.yaml
+PV1_NAME=pv1
+PVC1_FILE=pvc1.yaml
+PVC1_NAME=pvc1
+POD=nginx
+IMAGE=$POD
+PVC1_PATH=/tmp/pvc1
 
 # 0. Requirements
-#install metallb
-#helm repo add nginx-stable https://helm.nginx.com/stable
-#helm repo update
-#helm install ingress-nginx nginx-stable/nginx-ingress
+kubectl delete pod $POD --grace-period 0 --force
 
-# 1. Create deploys
-kubectl delete deploy $DEPLOY1 $DEPLOY2
-kubectl create deploy $DEPLOY1 --image $IMAGE1 --replicas $NUM_REPLICAS
-kubectl create deploy $DEPLOY2 --image $IMAGE2 --replicas $NUM_REPLICAS
+# 1. Create PVC
+kubectl delete pvc $PVC1_NAME --grace-period 0 --force
+kubectl create -f $PVC1_FILE
 
-# 2. Expose deploy as well as create LB service
-kubectl delete svc $SVC1 $SVC2
-kubectl expose deploy $DEPLOY1 --name $SVC1 --port $PORT --target-port $PORT
-kubectl expose deploy $DEPLOY2 --name $SVC2 --port $PORT --target-port $PORT
+# 2. Create PV
+kubectl delete pv $PV1_NAME --grace-period 0 --force
+kubectl create -f $PV1_FILE
 
-# 3. Create ingress
-kubectl delete ingress $INGRESS
-kubectl create ingress $INGRESS \
-  --class $INGRESS_CLASS \
-  --rule "public1.robertoarcomano.com/*=$SVC1:$PORT" \
-  --rule "public2.robertoarcomano.com/*=$SVC2:$PORT"
+# 3. Create POD
+kubectl run $POD --image $IMAGE --dry-run=client -o yaml | yq "
+.spec.volumes = [ { \"name\": \"$PVC1_NAME\", \"persistentVolumeClaim\": { \"claimName\": \"$PVC1_NAME\"} } ],
+.spec.containers[0].volumeMounts = [ { \"name\": \"$PVC1_NAME\", \"mountPath\": \"$PVC1_PATH\"  } ]
+" | kubectl create -f -
 
-# 4. Wait for replicas starting
-kubectl rollout status deploy $DEPLOY1
-kubectl rollout status deploy $DEPLOY2
+# 4. Wait for POD to become ready
+echo "Waiting for pod $POD to become ready"
+until kubectl get pod nginx -o yaml|yq ".status.phase"|grep Running; do echo -n "."; sleep 1; done
 
-# 5. Ingress test
-wget http://public1.robertoarcomano.com -O - -q
-wget http://public2.robertoarcomano.com -O - -q
+# 5. Test PV
+kubectl exec $POD -- bash -c "df -hT $PVC1_PATH"
