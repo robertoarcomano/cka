f8017f1834ea5f1181e4e5192e7ede6353664332
Roberto Arcomano <bertolinux@gmail.com>
2022-09-01 22:20:04 +0200

    helm. ok

--- 16. helm/start.sh
+++ 16. helm/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 0. Constants
-REPO_SRC=https://lead4good.github.io/lamp-helm-repository
-REPO_NAME=lamp
-CHART=k8s-lamp
+REPO_SRC=https://charts.bitnami.com/bitnami
+REPO_NAME=bitnami
+CHART=drupal
 STORAGE_CLASS=nfs-client
 
 # 1. Add lamp repository
@@ -12,8 +12,12 @@ helm repo add $REPO_NAME $REPO_SRC
 
 # 2. Install lamp
 helm uninstall $CHART
-helm install $CHART $REPO_NAME/$REPO_NAME --set persistence.storageClass=$STORAGE_CLASS
+helm install $CHART bitnami/$CHART --set global.storageClass=$STORAGE_CLASS --set mariadb.primary.persistence.storageClass=$STORAGE_CLASS
 
+# 3. Autentication data
+echo Username: user
+echo Password: $(kubectl get secret --namespace default drupal -o jsonpath="{.data.drupal-password}" | base64 -d)
 
-
-
+# 3. Connection link
+export SERVICE_IP=$(kubectl get svc --namespace default drupal -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+echo "Drupal URL: http://$SERVICE_IP/"
