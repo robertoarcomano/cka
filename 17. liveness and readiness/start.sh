ed1e393cb7aeb094200fb265c4757b772f5a9060
Roberto Arcomano <bertolinux@gmail.com>
2022-09-16 01:03:49 +0200

    liveness and readiness WIP3

--- 17. liveness and readiness/start.sh
+++ 17. liveness and readiness/start.sh
@@ -3,10 +3,12 @@
 # 0. Constants
 POD=busybox
 IMAGE=busybox
-
+START_DELAY=10
+WORKING_DELAY=10
+STARTED_FILE=/tmp/started
 # 1. Remove old pod e create new one
 kubectl delete pod $POD --grace-period 0 --force
-kubectl run $POD --image $IMAGE --dry-run=client -o yaml | yq '
-.spec.containers[0].command = ["/bin/sh","-c","while true; do echo 'ok'; sleep 1; done"]
-' | kubectl create -f -
+kubectl run $POD --image $IMAGE --dry-run=client -o yaml | yq "
+.spec.containers[0].args = [\"/bin/sh\",\"-c\",\"sleep $START_DELAY; touch $STARTED_FILE; sleep $WORKING_DELAY; rm $STARTED_FILE\"]
+" | kubectl create -f -
 
