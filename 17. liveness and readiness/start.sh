bd072b294154ae32814584c5c8fe9d2d8ee62fa6
Roberto Arcomano <bertolinux@gmail.com>
2022-09-15 22:53:29 +0200

    liveness and readiness WIP2

--- 17. liveness and readiness/start.sh
+++ 17. liveness and readiness/start.sh
@@ -5,7 +5,7 @@ POD=busybox
 IMAGE=busybox
 
 # 1. Remove old pod e create new one
-kubectl delete -f pod.yaml --grace-period 0 --force
+kubectl delete pod $POD --grace-period 0 --force
 kubectl run $POD --image $IMAGE --dry-run=client -o yaml | yq '
 .spec.containers[0].command = ["/bin/sh","-c","while true; do echo 'ok'; sleep 1; done"]
 ' | kubectl create -f -
