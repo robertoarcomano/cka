162b15d5f3a8d1e53871aa16d33f24df4c02594a
Roberto Arcomano <bertolinux@gmail.com>
2022-09-16 17:36:04 +0200

    21. master info ok.

---
+++ 21 master info/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+MASTER=$(kubectl get nodes --no-headers|grep control-plane|awk '{print $1}')
+MANIFEST_DIR=/etc/kubernetes/manifests
+
+# 1. Check processes
+ssh root@"$MASTER" find /etc/systemd/system/ | grep kube
+echo
+
+# 2. Check static pods
+ssh root@"$MASTER" find $MANIFEST_DIR | grep yaml
+echo
+
+# 3. Check running pods on the master
+kubectl get pods -A|grep "$MASTER"
+echo
+
+# 3. Check dns service
+kubectl get all -A|grep -i dns
