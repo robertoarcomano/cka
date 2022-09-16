984f8d8889dbf9afb9469a551073189475c66537
Roberto Arcomano <bertolinux@gmail.com>
2022-09-16 23:30:33 +0200

    23 rbac sa. ok

--- 23 rbac sa/start.sh
+++ 23 rbac sa/start.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 # 0. Constants
-MASTER=$(kubectl get nodes --no-headers|grep control-plane|awk '{print $1}')
+SA=processor
+NS=project-hamster
+ROLE=$SA
+ROLEBINDING=$ROLE
 
+# 0. Delete rolebinding, sa, role, ns
+kubectl delete rolebinding -ns $NS $ROLEBINDING --grace-period 0 --force
+kubectl delete sa $SA --grace-period 0 --force
+kubectl delete role $ROLE --grace-period 0 --force
+kubectl delete ns $NS --grace-period 0 --force
+echo
 
-# 1. Temporary kill the scheduler
+# 1. Create NS as well as SA
+kubectl create ns $NS
+kubectl create sa $SA -n $NS
 
+# 2. Create Role as well as RoleBinding
+kubectl create role -n $NS $ROLE --verb "create" --resource "secrets,configmaps"
+kubectl create rolebinding -n $NS $ROLEBINDING  --role=$ROLE --serviceaccount $NS:$SA
