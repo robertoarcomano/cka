d451ccd397fbd77153b2c6ae183bbc88e0b755cf
Roberto Arcomano <bertolinux@gmail.com>
2022-09-17 19:45:29 +0200

    26. multi container and shared volume: ok.

--- 26 multi container and shared volume/start.sh
+++ 26 multi container and shared volume/start.sh
@@ -6,6 +6,11 @@
 kubectl replace -f pod.yaml --grace-period 0 --force
 echo
 
-# 2. Check ds
-# Wait until ready
+# 2. Wait until ready
 until kubectl get pod multi-container-playground | grep "Running" -c|grep 1; do sleep 1; done
+
+# 3. Checks output
+kubectl exec -it multi-container-playground -c c1 -- env|grep MY_NODE_NAME
+kubectl exec -it multi-container-playground -c c2 -- ps xal|grep "date >> "|grep -v grep
+kubectl logs multi-container-playground -c c3
+
