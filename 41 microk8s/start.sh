af208ad94fd410a172b870456e3bdd8ff6a98525
Roberto Arcomano <info@robertoarcomano.com>
2023-08-03 12:38:57 +0200

    Added ubuntu

--- 41 microk8s/start.sh
+++ 41 microk8s/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # 0. Constants
+KUBECTL="microk8s kubectl"
+POD_FILE="ubuntu.yaml"
 
 # 1. Install microk8s
 sudo apt update
@@ -12,4 +14,7 @@ microk8s status
 
 # 3. Show usage
 echo "Command to connect to microk8s:"
-echo "microk8s kubectl
+echo "microk8s kubectl"
+
+# 4. Pod Test
+$KUBECTL create -f $POD_FILE
